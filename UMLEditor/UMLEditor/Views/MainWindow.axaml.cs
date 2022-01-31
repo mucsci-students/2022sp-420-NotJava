cf5aed0aec05d23a60162dca1522bfd4dd072fe4
Gavin Fry <94399746+gfry99@users.noreply.github.com>
2022-01-30 20:43:44 -0500

    Implemented List Button functionality.

--- UMLEditor/UMLEditor/Views/MainWindow.axaml.cs
+++ UMLEditor/UMLEditor/Views/MainWindow.axaml.cs
@@ -19,10 +19,31 @@ namespace UMLEditor.Views
             AvaloniaXamlLoader.Load(this);
         }
 
-        private void Exit_OnClick(object sender, RoutedEventArgs e)
+        private void ExitB_OnClick(object sender, RoutedEventArgs e)
         {
             Environment.Exit(0);
         }
-        
+
+        private void HelpB_OnClick(object sender, RoutedEventArgs e)
+        {
+            
+            TextBox OutputBox = this.FindControl<TextBox>("OutputBox");
+            OutputBox.Text = 
+                "New Class: Add a new class to the diagram" + 
+                "\nDelete Class: Delete an existing class" +
+                "\nRename Class: Rename an existing class" +
+                "\nAdd Relationship: Add a relationship between classes" +
+                "\nDelete Relationship: Delete an existing relationship" +
+                "\nAdd Attribute: Add an attribute to an existing Class" +
+                "\nDelete Attribute: Delete an existing class attribute" +
+                "\nRename Attribute: Rename an existing attribute" +
+                "\nSave: Save your progress" +
+                "\nLoad: Load a previously saved diagram" +
+                "\nList Classes: List all existing classes" +
+                "\nList Attributes: List all attributes of a class" +
+                "\nList Relationships: List all relationships of a class";
+            
+            
+        }
     }
 }
