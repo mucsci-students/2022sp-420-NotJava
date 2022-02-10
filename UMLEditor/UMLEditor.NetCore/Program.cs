24a549973dcf9cc6c6df35649e83c7f91edfc917
Robunnin <89474048+Robunnin@users.noreply.github.com>
2022-02-10 17:44:59 -0500

    Added AttributeTester/ Code cleanup
    Added: AttributeTester, TestingSled (that invokes all tests). Tweaked the project's config to allow running both CLI and GUI. Cleaned up main to separate args length checking from argument parsing

--- UMLEditor/UMLEditor.NetCore/Program.cs
+++ UMLEditor/UMLEditor.NetCore/Program.cs
@@ -1,27 +1,80 @@
-﻿sing System;
+﻿using System;
 using Avalonia;
 using Avalonia.ReactiveUI;
+using Avalonia.Threading;
+using UMLEditor.Classes;
+using UMLEditor.Testing;
 
 namespace UMLEditor.NetCore
 {
     class Program
     {
+        
+        /// <summary>
+        /// Custom defined exit codes
+        /// </summary>
+        private enum CustomExitCodes
+        {
+            
+            ERROR_UNIMPLEMENTED = -100,
+            ERROR_FAILED_UNIT_TEST = -101
+
+        }
+
         // Initialization code. Don't use any Avalonia, third-party APIs or any
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
         [STAThread]
         public static void Main(string[] args)
         {
-            if (args.Length != 0 && args[0] == "-test")
+            
+            if (args.Length >= 1)
             {
 
-            }
+                string providedArg = args[0];
 
-            else
-            {
-                BuildAvaloniaApp()
-                    .StartWithClassicDesktopLifetime(args);
+                switch (providedArg)
+                {
+                    
+                    case("-test"):
+                        
+                        // Unit testing mode
+                        try { TestingSled.RunTests(); }
+
+                        catch (Exception e)
+                        {
+
+                            string message = string.Format("FATAL: Exception when running unit tests: {0}\n" +
+                                                           "Stack Trace:\n{1}", e.Message, e.StackTrace);
+                            
+                            TestingSled.PrintColoredLine(message, TestingSled.ERROR_COLOR);
+                            Environment.ExitCode = (int)CustomExitCodes.ERROR_FAILED_UNIT_TEST;
+                            
+                        }
+                        
+                        Console.Write("Press enter to exit... ");
+                        Console.ReadLine();
+                        break;
+                    
+                    case("-cli"):
+                        
+                        // CLI mode
+                        Environment.Exit((int)CustomExitCodes.ERROR_UNIMPLEMENTED);
+                        break;
+                    
+                    default:
+                        Console.WriteLine("Invalid argument provided: {0}", providedArg);
+                        break;
+                    
+                }
+                
+                return;
+
             }
+            
+            // GUI mode is the default mode
+            BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+            
         }
 
         // Avalonia configuration, don't remove; also used by visual designer.
@@ -30,5 +83,6 @@ namespace UMLEditor.NetCore
                 .UsePlatformDetect()
                 .LogToTrace()
                 .UseReactiveUI();
+        
     }
 }
---
+++ UMLEditor/UMLEditor/Testing/TestingSled.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace UMLEditor.Testing;
+
+/// <summary>
+/// A tool that will invoke the unit tests
+/// </summary>
+public static class TestingSled
+{
+
+    // Text colors for different statuses
+    public static readonly ConsoleColor WARNING_COLOR = ConsoleColor.Yellow;
+    public static readonly ConsoleColor SUCCESS_COLOR = ConsoleColor.Green;
+    public static readonly ConsoleColor ERROR_COLOR = ConsoleColor.Red;
+    
+    /// <summary>
+    /// Invokes all unit tests
+    /// </summary>
+    public static void RunTests()
+    {
+        
+        PrintColoredLine("Running unit tests...\n", WARNING_COLOR);
+
+        // Test AttributeObject class
+        ///////////////////////////////////////////////////////////////////////////////////////
+        
+        PrintColoredLine("Testing AttributeObject...", SUCCESS_COLOR);
+        AttributeTester.RunTests();
+        PrintColoredLine("\nAttributeObject has passed all tests\n", SUCCESS_COLOR);
+        
+        ///////////////////////////////////////////////////////////////////////////////////////
+        
+        // Test...
+        
+        PrintColoredLine("All tests passed", SUCCESS_COLOR);
+
+    }
+
+    /// <summary>
+    /// Prints the provided text in a specified color.
+    /// </summary>
+    /// <param name="text">The text to print.</param>
+    /// <param name="inColor">The color you want the text printed in.</param>
+    public static void PrintColoredLine(string text, ConsoleColor inColor = ConsoleColor.White)
+    {
+
+        ConsoleColor oldTextColor = Console.ForegroundColor;
+
+        Console.ForegroundColor = inColor;
+        Console.WriteLine(text);
+        
+        Console.ForegroundColor = oldTextColor;
+
+    }
+    
+}
