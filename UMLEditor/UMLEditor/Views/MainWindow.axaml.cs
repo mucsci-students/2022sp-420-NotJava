9556b39a02abf9b7cbc036c7c5776c0e600f7c3c
Gavin Fry <94399746+gfry99@users.noreply.github.com>
2022-02-24 15:02:49 -0500

    Added Help Modal

--- UMLEditor/UMLEditor/Views/MainWindow.axaml.cs
+++ UMLEditor/UMLEditor/Views/MainWindow.axaml.cs
@@ -138,23 +138,13 @@ namespace UMLEditor.Views
 
         private void HelpB_OnClick(object sender, RoutedEventArgs e)
         {
-            ClearInputBox();            
-            
-            // Output list of possible commands.
-            _outputBox.Text = 
-                "New Class: Add a new class to the diagram" + 
-                "\nDelete Class: Delete an existing class" +
-                "\nRename Class: Rename an existing class" +
-                "\nAdd Relationship: Add a relationship between classes" +
-                "\nDelete Relationship: Delete an existing relationship" +
-                "\nAdd Attribute: Add an attribute to an existing Class" +
-                "\nDelete Attribute: Delete an existing class attribute" +
-                "\nRename Attribute: Rename an existing attribute" +
-                "\nSave: Save your progress" +
-                "\nLoad: Load a previously saved diagram" +
-                "\nList Classes: List all existing classes" +
-                "\nList Attributes: List all attributes of a class" +
-                "\nList Relationships: List all relationships of a class";
+            RaiseAlert(
+                "Help", 
+                "",
+                "", // TODO Figure out the wording for the new help functionality
+                AlertIcon.INFO
+            );
+            return;
         }
         
         private void List_Classes_OnClick(object sender, RoutedEventArgs e)
