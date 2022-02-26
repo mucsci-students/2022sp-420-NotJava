b2f62ba3aeb6de30e390e099a9841fd46d4b316d
Robunnin <89474048+Robunnin@users.noreply.github.com>
2022-02-25 22:15:30 -0500

    Initial Class Representation
    Basic classes added for representing classes

---
+++ UMLEditor/UMLEditor/Views/ClassBox.axaml.cs
@@ -0,0 +1,33 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using System.Collections.Generic;
+
+namespace UMLEditor.Views;
+
+public class ClassBox : UserControl
+{
+    public ClassBox()
+    {
+        InitializeComponent();
+
+        StackPanel vs = this.FindControl<StackPanel>("FieldsArea");
+        vs.Children.Add(new FieldContainer());
+        vs.Children.Add(new FieldContainer());
+        vs.Children.Add(new FieldContainer());
+        
+        /*
+        vs.Content = new FieldContainer[]
+        {
+            new FieldContainer(),
+            new FieldContainer()
+        };
+        */
+
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
---
+++ UMLEditor/UMLEditor/Views/FieldContainer.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace UMLEditor.Views;
+
+public class FieldContainer : UserControl
+{
+    public FieldContainer()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
