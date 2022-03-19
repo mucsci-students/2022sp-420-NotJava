61d5d9a8fb1226fe71868c38b299b2cd4f65c28c
Daniel <dmforeacre@gmail.com>
2022-03-18 23:22:45 -0400

    Added TimeMachine class
    TimeMachine is a doubly linked list to track the history of diagram states that enables traversing forwards and backwards.

---
+++ UMLEditor/UMLEditor/Classes/TimeMachine.cs
@@ -0,0 +1,134 @@
+﻿namespace UMLEditor.Classes;
+
+/// <summary>
+/// Class to represent the action history of the UML program
+/// </summary>
+public static class TimeMachine
+{
+    /// <summary>
+    /// Node class to represent a single state in the time machine history.
+    /// </summary>
+    public class Node
+    {
+        /// <summary>
+        /// Reference to previous node
+        /// </summary>
+        public Node? PrevNode;
+        /// <summary>
+        /// Diagram at this node
+        /// </summary>
+        public Diagram? StateDiagram;
+        /// <summary>
+        /// Reference to next node
+        /// </summary>
+        public Node? NextNode;
+    }
+    /// <summary>
+    /// References to the beginning, end, and current state of the time machine
+    /// </summary>
+    public static Node? Head, Tail, Current;
+    /// <summary>
+    /// Counter for number of states in the time machine
+    /// </summary>
+    public static int? Size;
+
+    /// <summary>
+    /// Initializes the time machine to an empty list
+    /// </summary>
+    public static void Initialize()
+    {
+        Head = null;
+        Tail = Head;
+        Current = Tail;
+        Size = 0;
+    }
+
+    /// <summary>
+    /// Adds a new diagram to the end of the list. Erases redo history if diagram is added to middle of list.
+    /// </summary>
+    /// <param name="d">Diagram to add</param>
+    public static void AddState(Diagram d)
+    {
+        // If in the middle of the list clears out all future nodes
+        if (Current?.NextNode is not null)
+        {
+            Node? temp = Current.NextNode;
+            while (temp is not null)
+            {
+                Node? next = temp.NextNode;
+                temp.StateDiagram = null;
+                temp.PrevNode = null;
+                temp.NextNode = null;
+                temp = next;
+                Size--;
+            }
+        }
+        
+        // Add new node to end of list
+        Node newNode = new Node();
+        newNode.StateDiagram = new Diagram(d);
+        newNode.PrevNode = Current;
+        newNode.NextNode = null;
+        if (Current is not null)
+        {
+            Current.NextNode = newNode;
+        }
+        Tail = newNode;
+        Size++;
+        Current = Tail;
+        if (Size == 1)
+        {
+            Head = newNode;
+        }
+    }
+
+    /// <summary>
+    /// Moves to next state in the time machine (redo)
+    /// </summary>
+    /// <returns>The next diagram, or null if there are none</returns>
+    public static Diagram? NextState()
+    {
+        if (Current != Tail)
+        {
+            Current = Current?.NextNode;
+            return Current?.StateDiagram;
+        }
+
+        return null;
+    }
+
+    /// <summary>
+    /// Moves to previous state in the time machine (undo)
+    /// </summary>
+    /// <returns>The previous diagram, or null if there are none</returns>
+    public static Diagram? PreviousState()
+    {
+        if (Current != Head)
+        {
+            Current = Current?.PrevNode;
+            return Current?.StateDiagram;
+        }
+
+        return null;
+    }
+
+    /// <summary>
+    /// Clears entire history. Dereferences all nodes.
+    /// </summary>
+    public static void ClearTimeMachine()
+    {
+        Current = Head;
+        while (Current is not null)
+        {
+            Node? next = Current.NextNode;
+            Current.StateDiagram = null;
+            Current.PrevNode = null;
+            Current.NextNode = null;
+            Current = next;
+        }
+        Head = null;
+        Tail = Head;
+        Current = Tail;
+        Size = 0;
+    }
+}
---
+++ UnitTests/TimeMachineUnitTests.cs
@@ -0,0 +1,77 @@
+﻿using NUnit.Framework;
+using UMLEditor.Classes;
+using UMLEditor.Exceptions;
+
+namespace UnitTests;
+
+[TestFixture]
+public class TimeMachineUnitTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void InitializeTimeMachineTest()
+    {
+        TimeMachine.Initialize();
+        Assert.AreEqual(0, TimeMachine.Size);
+        Assert.AreEqual(TimeMachine.Head,TimeMachine.Tail);
+        Assert.AreEqual(TimeMachine.Current,TimeMachine.Tail);
+        Assert.AreEqual(null, TimeMachine.Current);
+    }
+
+    [Test]
+    public void AddStateTest()
+    {
+        TimeMachine.Initialize();
+        Diagram d1 = new Diagram();
+        TimeMachine.AddState(d1);
+        Assert.AreEqual(1, TimeMachine.Size);
+        Assert.AreEqual(TimeMachine.Current, TimeMachine.Head);
+        Assert.AreEqual(TimeMachine.Current, TimeMachine.Tail);
+        Diagram d2 = new Diagram();
+        TimeMachine.AddState(d2);
+        Assert.AreEqual(2,TimeMachine.Size);
+        Assert.AreEqual(TimeMachine.Head, TimeMachine.Current.PrevNode);
+        Assert.AreEqual(TimeMachine.Tail, TimeMachine.Current);
+        Assert.AreEqual(null, TimeMachine.Current.NextNode);
+    }
+
+    [Test]
+    public void IncrementTest()
+    {
+        TimeMachine.Initialize();
+        Diagram d1 = new Diagram();
+        d1.AddClass("Diagram1Class");
+        Diagram d2 = new Diagram();
+        d2.AddClass("Diagram2Class");
+        TimeMachine.AddState(d1);
+        TimeMachine.AddState(d2);
+        // Go back one step
+        Assert.AreEqual(TimeMachine.Head.StateDiagram, TimeMachine.PreviousState());
+        // Try to go back another, but at beginning of list
+        Assert.AreEqual(null, TimeMachine.PreviousState());
+        Diagram d3 = new Diagram();
+        d3.AddClass("Diagram3Class");
+        // Create new future
+        TimeMachine.AddState(d3);
+        Assert.AreEqual("Diagram1Class", TimeMachine.PreviousState().GetClassByName("Diagram1Class").ClassName);
+        Assert.AreEqual("Diagram3Class",TimeMachine.NextState().GetClassByName("Diagram3Class").ClassName);
+        Assert.AreEqual(null, TimeMachine.NextState());
+    }
+
+    [Test]
+    public void ClearTest()
+    {
+        TimeMachine.Initialize();
+        Diagram d1 = new Diagram();
+        TimeMachine.AddState(d1);
+        TimeMachine.ClearTimeMachine();
+        Assert.AreEqual(null, TimeMachine.Head);
+        Assert.AreEqual(null, TimeMachine.Current);
+        Assert.AreEqual(null, TimeMachine.Tail);
+        Assert.AreEqual(0, TimeMachine.Size);
+    }
+}
