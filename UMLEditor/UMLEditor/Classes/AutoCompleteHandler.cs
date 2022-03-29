01b366f5b232f35a138afcc1a4bd0b808527e6f5
Matthew Young <mayoung@millersville.edu>
2022-03-28 23:29:41 -0400

    Added comments for handeler functions

--- UMLEditor/UMLEditor/Classes/AutoCompleteHandler.cs
+++ UMLEditor/UMLEditor/Classes/AutoCompleteHandler.cs
@@ -9,20 +9,20 @@ namespace UMLEditor.Classes;
 public class AutoCompleteHandler : IAutoCompleteHandler
 {
     /// <summary>
-    /// 
+    /// Separate different parts of list
     /// </summary>
     public char[] Separators { get; set; } = new char[] { ' ' };
 
     /// <summary>
-    /// 
+    /// List of commands supported
     /// </summary>
     public string[] SupportedCommands { get; set; } = Array.Empty<string>();
 
     /// <summary>
-    /// 
+    /// Gets suggestions given what user has typed
     /// </summary>
-    /// <param name="index"></param>
-    /// <param name="forLine"></param>
+    /// <param name="index">index of where user typed up to in string</param>
+    /// <param name="forLine">Line to attempt to complete</param>
     /// <returns></returns>
     public string[] GetSuggestions(int index, string forLine)
     {
@@ -39,11 +39,11 @@ public class AutoCompleteHandler : IAutoCompleteHandler
     }
 
     /// <summary>
-    /// 
+    /// Returns the last matching index of a string
     /// </summary>
-    /// <param name="first"></param>
-    /// <param name="second"></param>
-    /// <returns></returns>
+    /// <param name="first">The first string</param>
+    /// <param name="second">The second string</param>
+    /// <returns>The last matching index</returns>
     private int LastMatchingIndex(string first, string second)
     {
         int result = -1;
@@ -65,16 +65,22 @@ public class AutoCompleteHandler : IAutoCompleteHandler
     }
     
     /// <summary>
-    /// 
+    /// Checks if a string is a likely candidiate
     /// </summary>
-    /// <param name="toTest"></param>
-    /// <param name="candidate"></param>
-    /// <returns></returns>
+    /// <param name="toTest">String to test</param>
+    /// <param name="candidate">possible candidate to test against</param>
+    /// <returns>Returns true if the candidate is a likely candidate</returns>
     private bool IsLikelyCandidate(string toTest, string candidate)
     {
         return LastMatchingIndex(toTest, candidate) != -1;
     }
 
+    /// <summary>
+    /// Gets suggestions for rest of command given partial typed command
+    /// </summary>
+    /// <param name="forLine">Partially completed command from user</param>
+    /// <param name="index">Index of where user stopped typing</param>
+    /// <returns>array of possible commands</returns>
     public string[] GetSuggestions(string forLine, int index)
     {
         List<string> candidates = new List<string>();
